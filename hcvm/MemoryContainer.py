dac35bb3730a4074f2f9c99e2adb3ad9f542b378
CodingTrue <codingtrue@web.de>
2024-01-14 17:23:05 +0100

    Fixed max_static_value implmentation

--- hcvm/MemoryContainer.py
+++ hcvm/MemoryContainer.py
@@ -2,7 +2,7 @@ class MemoryConatiner:
     def __init__(self, size: int = 4096, max_static_value: int = 0):
         self.size = size
         self.memory = bytearray(size)
-        self.max_static_value = max_static_value
+        self.max_static_value = 2**(size*4) if max_static_value == 0 else max_static_value
 
     def get_byte_range(self, start_address: int = 0, address_range: int = 1) -> list[int]:
         return [self.memory[(start_address + x) % self.size] for x in range(address_range)]
@@ -24,13 +24,13 @@ class MemoryConatiner:
             self.memory[(start_address + i) % self.size] = values[i]
 
     def get_static_value(self) -> int:
-        return int.from_bytes(self.memory, byteorder="big", signed=False)
+        return int.from_bytes(self.memory, byteorder="big", signed=False) % self.max_static_value
 
     def set_static_value(self, value: int = 0):
         self.memory = int.to_bytes(value, byteorder="big", signed=False, length=value)
 
     def increase_static_value(self, value: int = 0):
-        target_value = (self.get_static_value() + value) % 0xffff
+        target_value = (self.get_static_value() + value) % self.max_static_value
         self.memory = int.to_bytes(target_value, byteorder="big", signed=False, length=target_value)
 
     def decrease_static_value(self, value: int = 0):
